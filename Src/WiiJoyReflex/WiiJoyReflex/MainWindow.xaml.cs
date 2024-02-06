eaa10f1d1e652dcfbca2f04f8f25625569d5a829
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-06 05:33:20 +0100

    Modify quote

--- Src/WiiJoyReflex/WiiJoyReflex/MainWindow.xaml.cs
+++ Src/WiiJoyReflex/WiiJoyReflex/MainWindow.xaml.cs
@@ -46,7 +46,7 @@ namespace WiiJoyReflex
         }
         public Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            string dllName = args.Name.Contains(',') ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(",")) : args.Name.Replace(".dll", "");
             dllName = dllName.Replace(".", "_");
             if (dllName.EndsWith("_resources"))
                 return null;
